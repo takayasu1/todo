0a1e756e106313dc48ca2b9afec4da43b16c84e0
t_koura <t_koura@bs-ja.co.jp>
2019-07-18 20:44:17 +0900

    詳細画面への遷移処理を実装

--- src/Detail.js
+++ src/Detail.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import App from './App';
+import firebase from './firebaseConfig';
 
 export default class Detail extends Component {
+  async componentWillMount() {
+    const todo = await firebase
+                        .database()
+                        .ref(this.props.id)
+                        .once('value')
+                        .then(snapshot => snapshot.val());
+    this.setState({todo: todo});
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       page: '',
+      todo: [],
     }
   }
 
@@ -13,59 +24,31 @@ export default class Detail extends Component {
     this.setState({page: val});
   }
 
+  edit() {
+    // TODO
+  }
+
   render() {
-    let Content = null;
-
-    switch(this.state.page) {
-      case 'top':
-        Content = (() => <App page="top" />);
-        break;
-      case 'list':
-        Content = (() => <App page="list" />);
-        break;
-      case 'add':
-        Content = (() => <App page="add" />);
-        break;
-      case 'index':
-        Content = (() => <App page="login" />);
-        break;
-      default:
-        Content = (() => (
-          <div>
-            <div name="header">
-              <p>
-                <li onClick={this.set.bind(this, 'top')}>トップ画面へ</li>
-                <li onClick={this.set.bind(this, 'list')}>一覧画面</li>
-                <li onClick={this.set.bind(this, 'add')}>新規追加</li>
-                <li onClick={this.set.bind(this, 'index')}>ログアウト</li>
-              </p>
-            </div>
-
-            <div name="title">詳細画面</div>
-            <div>
-              <div>
-                TODO ID : 1
-              </div>
-              <div>
-                TODO タイトル : sampleタイトル
-              </div>
-              <div>
-                TODO 詳細説明 :
-              </div>
-              <div>
-                登録画像 : <img alt="" src="https://placehold.jp/150x150.png"  />
-              </div>
-            </div>
-            <div class="">
-              <button>編集</button>
-            </div>
-          </div>
-        ));
-    }
-
     return (
       <div>
-        <Content />
+        <div name="title">詳細画面</div>
+        <div>
+          <div>
+            TODO ID : {this.props.id}
+          </div>
+          <div>
+            TODO タイトル : {this.state.todo["title"]}
+          </div>
+          <div>
+            TODO 詳細説明 : {this.state.todo["description"]}
+          </div>
+          <div>
+            登録画像 : <img alt="" src="https://placehold.jp/150x150.png"  />
+          </div>
+        </div>
+        <div>
+          <button onClick={this.edit.bind(this)}>編集</button>
+        </div>
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -11,9 +11,6 @@ export default class List extends Component {
                         .ref()
                         .once('value')
                         .then(snapshot => snapshot.val());
-    //console.log(todoList);
-    //this.setState({todoList: Object.entries(todoList)});
-    //this.setState({todoList: Object.keys(todoList).map(function (key) {return todoList[key]})})
     this.setState({todoList: Object.entries(todoList).map(([key, value]) => ({key, value}))});
   }
 
@@ -23,22 +20,21 @@ export default class List extends Component {
       page: '',
       todoList: [],
       items: [],
+      id: '',
     }
   }
 
-  set(val) {
-    this.setState({page: val});
+  set(id) {
+    this.setState({id: id});
   }
 
   setItems(i, key) {
-    console.log(i + " " + key);
     const itemsCopy = this.state.items.slice();
     itemsCopy[i] = key;
     this.setState({items: itemsCopy});
   }
 
   async remove(key) {
-    //console.log(firebase.database);
     firebase.database().ref(key).remove();
 
     const todoList = await firebase
@@ -53,83 +49,31 @@ export default class List extends Component {
     }
   }
 
-  render() {
-    return(
-      <div>
-        <div>
-          <div name="title">検索</div>
-          <div>タイトル : <input type="text" name="title" value="" /></div>
-          <div>説明 内容 : <input type="text" name="description" value="" /></div>
-          <div>
-            <button type="button" name="button">検索</button>
-          </div>
-        </div>
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>KEY</th>
-                <th>TODO ID</th>
-                <th>TODO タイトル</th>
-                <th>TODO 説明</th>
-                <th>詳細</th>
-                <th>削除</th>
-              </tr>
-            </thead>
-            <tbody>
-              {console.log(this.state.todoList)}
-              {this.state.todoList.map((data, i) => (
-                <tr>
-                  <td>{this.setItems.bind(this, i, data.key)}</td>
-                  <td>{i+1}</td>
-                  <td>{data.value.description}</td>
-                  <td>{data.value.title}</td>
-                  <td>sample update</td>
-                  <td><button name="detailButton" onClick={this.set.bind(this, 'detail')}>詳細</button></td>
-                  <td><button onClick={this.remove.bind(this, data.key)}>削除</button></td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    )
+  search() {
+    // TODO
   }
-  /*
+
   render() {
-    let Content = null;
-    //console.log(this.state.todoList);
-    switch(this.state.page) {
-      case 'top':
-        Content = (() => <App page="top" />);
-        break;
-      case 'list':
-        Content = (() => <App page="list" />);
-        break;
-      case 'add':
-        Content = (() => <App page="add" />);
-        break;
-      case 'index':
-        Content = (() => <App page="login" />);
-        break;
-      case 'detail':
-        Content = (() => <Detail />);
-        break;
-      default:
-        Content = (() => (
+    if (this.state.id !== '') {
+      return (
+        <Detail id={this.state.id} />
+      )
+    } else {
+        return (
           <div>
             <div>
               <div name="title">検索</div>
               <div>タイトル : <input type="text" name="title" value="" /></div>
               <div>説明 内容 : <input type="text" name="description" value="" /></div>
               <div>
-                <button type="button" name="button">検索</button>
+                <button onClick={this.search.bind(this)}>検索</button>
               </div>
             </div>
             <div>
               <table>
                 <thead>
                   <tr>
+                    <th>NO</th>
                     <th>TODO ID</th>
                     <th>TODO タイトル</th>
                     <th>TODO 説明</th>
@@ -138,29 +82,22 @@ export default class List extends Component {
                   </tr>
                 </thead>
                 <tbody>
+                  {console.log(this.state.todoList)}
                   {this.state.todoList.map((data, i) => (
                     <tr>
-                      <td>{i}</td>
-                      <td>{data.description}</td>
-                      <td>{data.name}</td>
-                      <td>sample update</td>
-                      <td><button name="detailButton" onClick={this.set.bind(this, 'detail')}>詳細</button></td>
-                      <td><button>削除</button></td>
+                      <td>{i+1}</td>
+                      <td>{data.key}</td>
+                      <td>{data.value.title}</td>
+                      <td>{data.value.description}</td>
+                      <td><button name="detailButton" onClick={this.set.bind(this, data.key)}>詳細</button></td>
+                      <td><button onClick={this.remove.bind(this, data.key)}>削除</button></td>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
           </div>
-        ));
-        break;
+        )
     }
-
-    return (
-      <div>
-        <Content />
-      </div>
-    )
   }
-  */
 }
